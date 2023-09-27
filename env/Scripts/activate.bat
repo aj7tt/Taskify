44753fef9614b0f72d426797a3bec6fa94611f82
ajit <aj7tt.dev@gmail.com>
2023-09-27 09:55:23 +0530

    changes

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Satyam\Desktop\aj7tt\Legistify\env
+set VIRTUAL_ENV=C:\Users\Satyam\Desktop\aj7tt\Taskify\env
 
 if not defined PROMPT set PROMPT=$P$G
 
